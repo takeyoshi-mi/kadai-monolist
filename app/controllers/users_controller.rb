bebca52222a2ecacd4514b78808c8b202cc61a5e
Takeyoshi <hi.its.take@tmizusawa.com>
2017-05-08 15:53:06 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
-class UsersController < ApplicationController
+class UsersController < ApplicationControlle
+  before_action :require_user_logged_in, only: [:show]
+  
   def show
     @user = User.find(params[:id])
   end
